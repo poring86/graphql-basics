38f2f00ea38c928d713fd1c31aafc9f4572cf70d
Matheus Lino <matheuslino86a@gmail.com>
2022-11-20 20:11:09 -0400

    spread

--- src/index.js
+++ src/index.js
@@ -206,9 +206,7 @@ const resolvers = {
 
       const user = {
         id: uuidv4(),
-        name: args.name,
-        email: args.email,
-        age: args.age,
+        ...args,
       };
 
       users.push(user);
@@ -223,10 +221,7 @@ const resolvers = {
       }
       const post = {
         id: uuidv4(),
-        title: args.title,
-        body: args.body,
-        published: args.published,
-        author: args.author,
+        ...args,
       };
       posts.push(post);
 
@@ -249,9 +244,7 @@ const resolvers = {
 
       const comment = {
         id: uuidv4(),
-        text: args.text,
-        author: args.author,
-        post: args.post,
+        ...args,
       };
 
       comments.push(comment);
