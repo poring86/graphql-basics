52e58de73677ab61b6ef40e4cf2dfec53224ae2f
Matheus Lino <matheuslino86a@gmail.com>
2022-11-24 21:36:13 -0400

    delete post

--- src/index.js
+++ src/index.js
@@ -96,6 +96,7 @@ const typeDefs = `
       createUser(data: CreateUserInput): User!
       deleteUser(id: ID!): User!
       createPost(data: CreatePostInput): Post!
+      deletePost(id: ID!): Post!
       createComment(data: CreateCommentInput): Comment!
     }
 
@@ -268,6 +269,19 @@ const resolvers = {
 
       return post;
     },
+    deletePost(parent, args, ctx, info) {
+      const postIndex = posts.findIndex((post) => post.id === args.id);
+
+      if (postIndex === -1) {
+        throw new Error("Post not found");
+      }
+
+      const deletedPost = posts.splice(postIndex, 1);
+
+      comments = comments.filter((comment) => comment.post !== args.id);
+
+      return deletedPost[0];
+    },
     createComment(parent, args, ctx, info) {
       const userExists = users.some((user) => user.id === args.data.author);
 
@@ -300,9 +314,7 @@ const resolvers = {
       });
     },
     comments(parent, args, ctx, info) {
-      // return comments;
       return comments.filter((comment) => {
-        // return parent.id === comment.post;
         return comment.post === parent.id;
       });
     },
