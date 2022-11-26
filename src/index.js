11440c6a909077ebf8f9a28a0b754e763a483495
Matheus Lino <matheuslino86a@gmail.com>
2022-11-26 17:35:07 -0400

    Load schema file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { createServer } from "@graphql-yoga/node";
 import { v4 as uuidv4 } from "uuid";
 
+import path from "path";
+import fs from "fs";
+
 let comments = [
   {
     id: "1",
@@ -79,72 +82,6 @@ let users = [
     age: 22,
   },
 ];
-
-const typeDefs = `
-    type Query{
-      users(query: String): [User!]!
-      posts(query: String): [Post!]!
-      greeting(name: String, position: String): String!
-      add(numbers: [Float!]!): Float!
-      grades: [Int!]!
-      me: User!
-      post: Post!
-      comments: [Comment!]!
-    }
-
-    type Mutation{
-      createUser(data: CreateUserInput): User!
-      deleteUser(id: ID!): User!
-      createPost(data: CreatePostInput): Post!
-      deletePost(id: ID!): Post!
-      createComment(data: CreateCommentInput): Comment!
-      deleteComment(id: ID!): Comment!
-    }
-
-    input CreateUserInput{
-      name: String!
-      email: String!
-      age: Int
-    }
-
-    input CreatePostInput{
-      title: String!
-      body: String!
-      published: Boolean!
-      author: ID!
-    }
-
-    input CreateCommentInput{
-      text: String! 
-      author: ID! 
-      post: ID!
-    }
-
-    type User{
-      id: ID!
-      name: String!
-      email: String!
-      age: Int
-      posts: [Post!]!
-      comments: [Comment]
-    }
-
-    type Post {
-      id: ID!
-      title: String!
-      body: String!
-      published: Boolean!
-      author: User!
-      comments: [Comment!]!
-  }
-
-    type Comment {
-      id: ID!
-      text: String!
-      author: User!
-      post: Post!
-  }
-`;
 
 const resolvers = {
   Query: {
@@ -364,7 +301,7 @@ const resolvers = {
 
 const server = new createServer({
   schema: {
-    typeDefs,
+    typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
     resolvers,
   },
 });
