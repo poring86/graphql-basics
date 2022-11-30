005a39e7f33e35e7ab1b6aba7802252c960fcf23
Matheus Lino <matheuslino86a@gmail.com>
2022-11-30 11:24:19 -0400

    Comments subscriptions

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -196,11 +196,16 @@ const Mutation = {
 
     db.comments.push(comment);
 
-    pubsub.publish(`comment ${args.data.post}`, { comment });
+    pubsub.publish(`comment ${args.data.post}`, {
+      comment: {
+        mutation: "CREATED",
+        data: comment,
+      },
+    });
 
     return comment;
   },
-  deleteComment(parent, args, { db }, info) {
+  deleteComment(parent, args, { db, pubsub }, info) {
     const commentIndex = db.comments.findIndex(
       (comment) => comment.id === args.id
     );
@@ -209,13 +214,20 @@ const Mutation = {
       throw new GraphQLYogaError("Comment not found");
     }
 
-    const deletedComment = db.comments.splice(commentIndex, 1);
+    const [comment] = db.comments.splice(commentIndex, 1);
 
     db.comments = db.comments.filter((comment) => comment.id !== args.id);
 
-    return deletedComment[0];
+    pubsub.publish(`comment ${comment.post}`, {
+      comment: {
+        mutation: "DELETED",
+        data: comment,
+      },
+    });
+
+    return comment;
   },
-  updateComment(parent, args, { db }, info) {
+  updateComment(parent, args, { db, pubsub }, info) {
     const { id, data } = args;
     const comment = db.comments.find((comment) => comment.id === id);
 
@@ -226,6 +238,13 @@ const Mutation = {
     if (typeof data.text === "string") {
       comment.text = data.text;
     }
+
+    pubsub.publish(`comment ${comment.post}`, {
+      comment: {
+        mutation: "UPDATED",
+        data: comment,
+      },
+    });
 
     return comment;
   },
