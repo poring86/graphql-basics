4733eb265989ffa02af6fa6fea8e3ffff2bd1033
Matheus Lino <matheuslino86a@gmail.com>
2022-12-13 13:42:29 -0400

    Fetch my posts

--- tests/post.test.ts
+++ tests/post.test.ts
@@ -1,6 +1,7 @@
 import "cross-fetch/polyfill";
 import { GraphQLClient, gql } from "graphql-request";
 import seedDatabase from "./utils/seedDatabase";
+import { userOne } from "./utils/seedDatabase";
 
 const client = new GraphQLClient("http://127.0.0.1:4000/graphql");
 
@@ -18,4 +19,19 @@ test("Should show published posts", async () => {
     const response = await client.request(query);
 
     expect(response.posts.length).toBeGreaterThan(0);
+});
+
+test("Should fetch user posts", async () => {
+    client.setHeader("authorization", `Bearer ${userOne.token}`);
+    const query = gql`
+        query {
+            myPosts {
+                id
+            }
+        }
+    `;
+
+    const response = await client.request(query);
+
+    expect(response.myPosts.length).toBe(2);
 });
